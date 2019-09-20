3d9c4a63196b003c70ab9fe907d4c733d2110a1c
aqu1les <felipebarros.dev@gmail.com>
2019-09-19 21:34:09 -0300

    Routes changed to work with Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import Header from "./components/Layout/Header";
 import Router from "./router";
 
 export default function App() {
-	return (
-		<BrowserRouter>
-			<Header />
-			<Router />
-		</BrowserRouter>
-	);
+    return (
+        <BrowserRouter>
+            <Router />
+        </BrowserRouter>
+    );
 }
--- src/router.js
+++ src/router.js
@@ -6,27 +6,30 @@ import Adverts from "./pages/Adverts";
 import Users from "./pages/Users";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
+import Layout from "./components/Layout";
 
 export default () => {
-	return (
-		<Switch>
-			<Route exact path="/" render={() => <Redirect to="/login" />} />
-			<Route
-				path="/login"
-				render={() =>
-					sessionStorage.getItem("authKey") ? (
-						<Redirect to="/dashboard" />
-					) : (
-						<Login />
-					)
-				}
-			/>
-			<Route path="/dashboard" component={Dashboard} />
-			<Route path="/users" component={Users} />
-			<Route path="/adverts" Component={Adverts} />
-			<Route path="/reports" Component={Reports} />
-			<Route path="/me" Component={Profile} />
-			<Route component={() => <h1>Page not found</h1>} />
-		</Switch>
-	);
+    return (
+        <Switch>
+            <Route exact path="/" render={() => <Redirect to="/login" />} />
+            <Route
+                path="/login"
+                render={() =>
+                    sessionStorage.getItem("authKey") ? (
+                        <Redirect to="/dashboard" />
+                    ) : (
+                            <Login />
+                        )
+                }
+            />
+            <Layout>
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/users" component={Users} />
+                <Route path="/adverts" component={Adverts} />
+                <Route path="/reports" component={Reports} />
+                <Route path="/me" component={Profile} />
+            </Layout>
+            <Route component={() => <h1>Page not found</h1>} />
+        </Switch>
+    );
 };
