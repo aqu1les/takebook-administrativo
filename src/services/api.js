b9be068379dfa12473ecb9f6e7e47154e02329f3
aqu1les <felipebarros.dev@gmail.com>
2019-09-19 21:35:20 -0300

    service updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000'
+    baseURL: "http://localhost:8000"
 });
 
 api.interceptors.request.use(config => {
-    const token = sessionStorage.getItem('authKey');
+    const token = sessionStorage.getItem("authKey");
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
     }
@@ -16,7 +16,9 @@ api.interceptors.response.use(response => response,
     error => {
         const { status } = error.response;
         if (status === 401) {
-            return window.location.pathname = '/login';
+            localStorage.removeItem("user_info");
+            sessionStorage.removeItem("authKey");
+            return window.location.pathname = "/login";
         }
     });
 
