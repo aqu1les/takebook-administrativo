b31cbf7fc59f128800f98f4e7d2b73cf4832c2ff
aqu1les <felipebarros.dev@gmail.com>
2019-09-21 01:53:56 -0300

    memory leak fixed

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { Wrapper } from "./style";
 import api from "../../services/api";
+import axios from 'axios';
 
 export default class Dashboard extends Component {
+    signal = axios.CancelToken.source();
     componentDidMount() {
         try {
             this.loadData();
@@ -11,25 +13,37 @@ export default class Dashboard extends Component {
         }
     }
     state = {
-        totalUsers: "loading",
-        totalBooks: "loading",
-        weeklyBooks: "loading",
+        totalUsers: 0,
+        totalBooks: 0,
+        weeklyBooks: 0,
         totalReports: 0,
-        totalAdverts: 0
+        totalAdverts: 0,
+        isLoading: false
     }
     loadData = async () => {
-        const totalUsers = await api.get("/users");
-        const totalBooks = await api.get("/books");
-        const weeklyBooks = await api.get("/books/week");
-        const totalReports = await api.get("/reports");
-        const totalAdverts = await api.get("/books/validate");
-        this.setState({
-            totalUsers: totalUsers.data.total,
-            totalBooks: totalBooks.data.total,
-            weeklyBooks: weeklyBooks.data.count,
-            totalReports: totalReports.data.total,
-            totalAdverts: totalAdverts.data.total
-        });
+        this.setState({ isLoading: true });
+        try {
+            const totalUsers = await api.get("/users", { cancelToken: this.signal.token });
+            const totalBooks = await api.get("/books", { cancelToken: this.signal.token });
+            const weeklyBooks = await api.get("/books/week", { cancelToken: this.signal.token });
+            const totalReports = await api.get("/reports", { cancelToken: this.signal.token });
+            const totalAdverts = await api.get("/books/validate", { cancelToken: this.signal.token });
+            if (totalUsers || totalBooks || weeklyBooks || totalReports || totalAdverts) {
+                return this.setState({
+                    totalUsers: totalUsers.data.total,
+                    totalBooks: totalBooks.data.total,
+                    weeklyBooks: weeklyBooks.data.count,
+                    totalReports: totalReports.data.total,
+                    totalAdverts: totalAdverts.data.total
+                });
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+    componentWillUnmount() {
+        this.signal.cancel('Api is being canceled');
     }
     render() {
         return (
