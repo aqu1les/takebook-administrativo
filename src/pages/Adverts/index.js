5cd5cd7f3d12e397cb0a3d48478b127f0684804f
aqu1les <felipebarros.dev@gmail.com>
2019-11-16 05:05:31 -0300

    fixed Adverts Page, added Modal - #TODO

--- src/pages/Adverts/index.js
+++ src/pages/Adverts/index.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
-import { Wrapper, Card, Header, Search, Main, Content } from './style';
-import api from '../../services/api';
-import searchIcon from '../../assets/icons/search.svg';
-import Book from '../../components/Adverts/Book';
+import React, { Component } from "react";
+import { Wrapper, Card, Header, Main, Content, ModalCard, ModalLeftSide, ModalDivider, ModalRightSide } from "./style";
+import api from "../../services/api";
+import Book from "../../components/Adverts/Book";
+import SearchField from "../../components/SearchField";
+import Modal from "../../components/Modal";
+import accept from "../../assets/icons/accept.svg";
+import refuse from "../../assets/icons/refuse.svg";
 
 export default class Adverts extends Component {
 
     state = {
-        adverts: []
+        adverts: [],
+        filtered: [],
+        filterByTitle: "",
+        modalOpen: false,
+        modalBook: {
+            id: null,
+            author: null,
+            count_likes: null,
+            covers_url: [{
+                id: null,
+                url: null
+            }],
+            description: null,
+            price: null,
+            title: null,
+            user: {}
+        }
     }
 
     componentDidMount() {
@@ -19,34 +38,98 @@ export default class Adverts extends Component {
     }
 
     loadData = async () => {
-        const response = await api.get('/books/validate');
+        const response = await api.get("/books/validate");
         console.log(response.data);
-        this.setState({ adverts: this.state.adverts.concat(response.data.data) });
-        return console.log(this.state);
+        this.setState({
+            adverts: this.state.adverts.concat(response.data.data),
+            filtered: this.state.adverts.concat(response.data.data)
+        });
+    }
+    handleChange = e => {
+        if (!e.target.value) {
+            return this.setState({
+                filterByTitle: e.target.value,
+                filtered: this.state.adverts
+            });
+        }
+        return this.setState({
+            filterByTitle: e.target.value,
+            filtered: this.state.adverts.filter(
+                (book) => {
+                    return book.title.toLowerCase()
+                        .indexOf(this.state.filterByTitle.toLowerCase()) !== -1;
+                }
+            )
+        });
+    }
+    searchAdvert = () => {
+        const title = this.state.filterByTitle;
+        const filtered = this.state.adverts.filter(ad => ad.title === title);
+        return this.setState({ filtered });
+    }
+    openModal = async (id, e) => {
+        await this.setState({ modalOpen: true, modalBook: this.state.adverts.find(ad => ad.id === id) });
+        return this.changeCover();
+    }
+    closeModal = () => {
+        return this.setState({ modalOpen: false });
+    }
+    changeCover = (index) => {
+        const x = document.getElementsByClassName("slide");
+        let slideIndex = index;
+        if (index >= 0) {
+            if (index >= x.length) slideIndex = 0;
+            for (let i = 0; i < x.length; i++) {
+                x[i].style.display = "none";
+            }
+            return x[slideIndex].style.display = "block";
+        } else {
+            if (x[0]) x[0].style.display = "block";
+        }
     }
 
     render() {
+        let body = this.state.filtered.length > 0 ?
+            this.state.filtered.map(ad => (
+                <Book book={ad} key={ad.id} onClick={(e) => this.openModal(ad.id, e)} />
+            ))
+            :
+            "Nenhum livro foi encontrado!";
+
         return (
             <Wrapper>
-                <h2>Anúncios</h2>
+                <h2 id="page">Anúncios</h2>
                 <Card>
                     <Header>
-                        <Search>
-                            <input type="text" name="filter" placeholder="Pesquisar" />
-                            <button onClick={this.searchAdvert}>
-                                <img src={searchIcon} alt="Search icon" />
-                            </button>
-                        </Search>
+                        <SearchField onChange={this.handleChange} onClick={this.searchAdvert} />
                     </Header>
                     <Main>
                         <h2>Resultado</h2>
                         <Content>
-                            {this.state.adverts.map(ad => (
-                                <Book book={ad} key={ad.id} />
-                            ))}
+                            {body}
                         </Content>
                     </Main>
                 </Card>
+                <Modal open={this.state.modalOpen} click={this.closeModal}>
+                    <ModalCard>
+                        <ModalLeftSide>
+                            <div id="covers">
+                                {this.state.modalBook.covers_url.map((cover, index) => (
+                                    <div key={index} className="slide">
+                                        <img src={cover.url} alt="Book" />
+                                        <button onClick={() => this.changeCover(index + 1)}></button>
+                                    </div>
+                                ))}
+                            </div>
+                            <div>
+                                <img src={refuse} alt="Refuse Button" />
+                                <img src={accept} alt="Accept Button" />
+                            </div>
+                        </ModalLeftSide>
+                        <ModalDivider />
+                        <ModalRightSide></ModalRightSide>
+                    </ModalCard>
+                </Modal>
             </Wrapper>
         );
     }
--- src/pages/Adverts/style.js
+++ src/pages/Adverts/style.js
@@ -5,6 +5,9 @@ const width920 = "@media only screen and (max-width: 920px)";
 export const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
+    ${width920} {
+        margin: 70px 0;
+    }
 `;
 export const Card = styled.div`
     width: 100%;
@@ -15,60 +18,111 @@ export const Card = styled.div`
     box-shadow: 0 0 12px rgba(0, 0, 0, 0.16);
     border-radius: 8px;
     background-color: #FBFBFB;
-    padding: 10px;
+    padding: 0 10px;
+    ${width920} {
+        height: 100%;
+        width: 100%;
+        padding: 0;
+        box-shadow: none;
+        margin: 0;
+        background-color: #FFF;
+    }
 `;
 export const Header = styled.header`
-    height: 20%;
-`;
-export const Search = styled.div`
-    width: 80%;
-    margin-top: 20px;
-    height: 50px;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
-    border-radius: 40px;
+    height: 15%;
     display: flex;
+    flex-direction: row;
+    justify-content: center;
     align-items: center;
-    background-color: white;
-
-    input {
-        border: none;
-        width: 80%;
-        height: 90%;
-        margin-left: 25px;
-        border-radius: 40px;
-        font-size: 18px;
-        &:focus, &:active {
-            outline: none;
-        }
-        &::placeholder {
-            font-size: 18px;
-        }
-    }
-
-    button {
-        background-color: #EFEFEF;
-        margin-right: 10px
-        margin-left: auto;
-        border: none;
-        height: 30px;
-        width: 80px;
-        border-radius: 40px;
-        img {
-            height: 24px;
-            width: 24px;
-        }
-    }
 `;
 export const Main = styled.main`
     height: 80%;
+    margin-left: 20px;
     h2 {
         margin-bottom: 10px;
     }
 `;
 export const Content = styled.section`
-    height: 100%;
+    height: 95%;
     width: 100%;
     display: flex;
     justify-content: center;
     flex-wrap: wrap;
+    overflow-y: scroll;
+
+    ::-webkit-scrollbar {
+        border-radius: 50px;
+        width: 5px;     
+    }
+    
+    /* Track */
+    ::-webkit-scrollbar-track {
+        box-shadow: inset 0 0 1px grey; 
+        border-radius: 50px;
+    }    
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+        background: #2BC1F3; 
+        border-radius: 10px;
+        margin: 50px 0;
+    }    
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+        opacity: 0.6;
+    }
 `;
+export const ModalCard = styled.section`
+    width: 810px;
+    height: 540px;
+    background-color: #FFFFFF;
+    border-radius: 25px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    position: absolute;
+    z-index: 2;
+`
+export const ModalLeftSide = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    div#covers {
+        div.slide {
+            position: relative;
+            width: 200px;
+            height: 300px;
+            display: none;
+            img {
+                width: 100%;
+                height: 100%;
+                border-radius: 10px;
+                box-shadow: 0 1px 6px rgba(0, 0, 0, 0.16);
+            }
+            button {
+                position: absolute;
+                background-color: #FB8C00;
+                bottom: 0;
+                left: 90px;
+                width: 20px;
+                height: 20px;
+                border-radius: 100px;
+                border: none;
+            }
+        }        
+    }    
+    div img {
+        height: 100px;
+        width: 100px;
+    }
+`
+export const ModalDivider = styled.div`
+    height: 430px;
+    width: 0;
+    border: 1px solid #C1C3C4;
+`
+export const ModalRightSide = styled.div`
+    height: 100%;
+`
